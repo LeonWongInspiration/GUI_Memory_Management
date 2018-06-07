d97cadc5f96414f410b4a308b33aaf315a0f504f
LeonWongInspiration <leonwong.inspiration@gmail.com>
2018-06-07 21:23:08 +0800

    Finish Project

--- src/cn/leonwong/memory/Controller.java
+++ src/cn/leonwong/memory/Controller.java
@@ -8,22 +8,228 @@ import javafx.scene.control.*;
 import java.util.*;
 
 public class Controller {
-    private boolean isFifoMode;
+    private int pageMissingCount;
 
-    private boolean firstInstructionSet;
+    private boolean isFifoMode;
 
     private ArrayList<Integer> instructionArray;
 
+    private int[] memoryCount;
+
+    private int executionCount;
+
     public Controller(){
-        this.firstInstructionSet = false;
         this.instructionList = new ListView<>();
         this.isFifoMode = true;
+        this.pageMissingCount = 0;
+        this.memoryCount = new int[4];
+        this.blockObservableLists = new ObservableList[4];
+        this.executionCount = 0;
     }
 
     private void initLabels(int firstInstruction, int nextInstruction){
         this.pageMissingLabel.setText("0");
         this.currentInstructionLabel.setText(Integer.toString(firstInstruction));
         this.nextInstructionLabel.setText(Integer.toString(nextInstruction));
+    }
+
+    private void updatePageMissing(){
+        ++this.pageMissingCount;
+        this.pageMissingLabel.setText(Integer.toString(pageMissingCount));
+    }
+
+    private void finishExecution(){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setContentText("Execution finished!");
+        alert.setTitle("Finish");
+
+        this.fifoButton.setDisable(false);
+        this.lruButton.setDisable(false);
+        this.createListButton.setDisable(false);
+
+        this.nextStepButton.setDisable(true);
+        this.nextFiveButton.setDisable(true);
+        this.runAllButton.setDisable(true);
+
+        this.currentInstructionLabel.setText("Finished");
+        this.nextInstructionLabel.setText("Finished");
+        alert.showAndWait();
+    }
+
+    private void oneStepFIFO(){
+        if (this.instructionArray.isEmpty()){
+            finishExecution();
+            return;
+        }
+        else if (this.instructionArray.size() == 1){
+            this.nextInstructionLabel.setText("No more instructions");
+            this.currentInstructionLabel.setText(Integer.toString(this.instructionArray.get(0)));
+        }
+        else {
+            this.nextInstructionLabel.setText(Integer.toString(this.instructionArray.get(1)));
+            this.currentInstructionLabel.setText(Integer.toString(this.instructionArray.get(0)));
+        }
+        int currentInstruction = this.instructionArray.get(0);
+
+        boolean pageNotMissing = false;
+        int pageNumber = 0;
+        for (int i = 0; i < 4; ++i){
+            if (!this.blockObservableLists[i].isEmpty() &&
+                    this.blockObservableLists[i].contains(currentInstruction)){
+                pageNotMissing = true;
+                pageNumber = i;
+                break;
+            }
+            if (memoryCount[i] > memoryCount[pageNumber]){
+                pageNumber = i;
+            }
+        }
+
+        if (pageNotMissing){
+            String result = "Instruction: " +
+                    Integer.toString(currentInstruction) +
+                    " found in " +
+                    "Block #" +
+                    Integer.toString(pageNumber);
+            this.historyList.getItems().add(0, result);
+        }
+        else {
+            for (int i = 0; i < 4; ++i){
+                if (this.blockObservableLists[i].isEmpty()) {
+                    pageNumber = i;
+                    break;
+                }
+            }
+            this.updatePageMissing();
+            String result = "Instruction: " +
+                    Integer.toString(currentInstruction) +
+                    " missing. " +
+                    "Moving its page to: " +
+                    "Block #" +
+                    Integer.toString(pageNumber);
+            this.historyList.getItems().add(0, result);
+            int start = currentInstruction / 10 * 10;
+            this.blockObservableLists[pageNumber].setAll(
+                    start,
+                    start + 1,
+                    start + 2,
+                    start + 3,
+                    start + 4,
+                    start + 5,
+                    start + 6,
+                    start + 7,
+                    start + 8,
+                    start + 9);
+            this.memoryCount[pageNumber] = 0;
+            for (int i = 0; i < 4; ++i){
+                if (!this.blockObservableLists[i].isEmpty())
+                    ++this.memoryCount[i];
+            }
+        }
+
+        this.instructionList.getItems().remove(0);
+    }
+
+    private void oneStepLRU(){
+        if (this.instructionArray.isEmpty()){
+            finishExecution();
+            return;
+        }
+        else if (this.instructionArray.size() == 1){
+            this.nextInstructionLabel.setText("No more instructions");
+            this.currentInstructionLabel.setText(Integer.toString(this.instructionArray.get(0)));
+        }
+        else {
+            this.nextInstructionLabel.setText(Integer.toString(this.instructionArray.get(1)));
+            this.currentInstructionLabel.setText(Integer.toString(this.instructionArray.get(0)));
+        }
+        int currentInstruction = this.instructionArray.get(0);
+
+        boolean pageNotMissing = false;
+        int pageNumber = 0;
+        for (int i = 0; i < 4; ++i){
+            if (!this.blockObservableLists[i].isEmpty() &&
+                    this.blockObservableLists[i].contains(currentInstruction)){
+                pageNotMissing = true;
+                pageNumber = i;
+                break;
+            }
+            if (memoryCount[i] < memoryCount[pageNumber]){
+                pageNumber = i;
+            }
+        }
+
+        if (pageNotMissing){
+            String result = "Instruction: " +
+                    Integer.toString(currentInstruction) +
+                    " found in " +
+                    "Block #" +
+                    Integer.toString(pageNumber);
+            this.historyList.getItems().add(0, result);
+        }
+        else {
+            for (int i = 0; i < 4; ++i){
+                if (this.blockObservableLists[i].isEmpty()) {
+                    pageNumber = i;
+                    break;
+                }
+            }
+            this.updatePageMissing();
+            String result = "Instruction: " +
+                    Integer.toString(currentInstruction) +
+                    " missing. " +
+                    "Moving its page to: " +
+                    "Block #" +
+                    Integer.toString(pageNumber);
+            this.historyList.getItems().add(0, result);
+            int start = currentInstruction / 10 * 10;
+            this.blockObservableLists[pageNumber].setAll(
+                    start,
+                    start + 1,
+                    start + 2,
+                    start + 3,
+                    start + 4,
+                    start + 5,
+                    start + 6,
+                    start + 7,
+                    start + 8,
+                    start + 9);
+        }
+
+        this.memoryCount[pageNumber] = this.executionCount;
+        this.instructionList.getItems().remove(0);
+    }
+
+    @FXML
+    private void oneStepButtonOnClickHandler(){
+        if (this.instructionArray.isEmpty())
+            return;
+        if (this.isFifoMode)
+            this.oneStepFIFO();
+        else
+            this.oneStepLRU();
+        ++this.executionCount;
+        this.executeCountLabel.setText(Integer.toString(this.executionCount));
+        if (this.instructionArray.isEmpty())
+            this.finishExecution();
+    }
+
+    @FXML
+    private void fiveStepsButtonOnClickHandler(){
+        System.out.println("5 Steps");
+        for (int i = 0; i < 5; ++i){
+            this.oneStepButtonOnClickHandler();
+        }
+    }
+
+    @FXML
+    private void runAllButtonOnClickHandler(){
+        this.nextStepButton.setDisable(true);
+        this.nextFiveButton.setDisable(true);
+        this.runAllButton.setDisable(true);
+        while (!this.instructionArray.isEmpty()){
+            this.oneStepButtonOnClickHandler();
+        }
     }
 
     @FXML
@@ -50,17 +256,46 @@ public class Controller {
             System.out.println("Error: bad first instruction number!");
         }
         else {
-            this.firstInstructionSet = true;
             RandomGenerator rg = new RandomGenerator(first);
             ArrayList<Integer> list = rg.getInstructions();
             ObservableList<Integer> observableList = FXCollections.observableList(list);
             this.instructionList.setItems(observableList);
+            this.instructionList.setEditable(false);
             this.instructionArray = list;
             System.out.println("Creating instruction list.");
             this.initLabels(list.get(0), list.get(1));
 
             this.fifoButton.setDisable(true);
             this.lruButton.setDisable(true);
+            this.createListButton.setDisable(true);
+
+            this.nextStepButton.setDisable(false);
+            this.nextFiveButton.setDisable(false);
+            this.runAllButton.setDisable(false);
+
+            this.block0List.setEditable(false);
+            this.block1List.setEditable(false);
+            this.block2List.setEditable(false);
+            this.block3List.setEditable(false);
+
+            this.historyList.setEditable(false);
+
+            for (int i = 0; i < 4; ++i){
+                this.blockObservableLists[i] = FXCollections.observableArrayList();
+            }
+            this.block0List.setItems(this.blockObservableLists[0]);
+            this.block1List.setItems(this.blockObservableLists[1]);
+            this.block2List.setItems(this.blockObservableLists[2]);
+            this.block3List.setItems(this.blockObservableLists[3]);
+
+            this.memoryCount = new int[]{0, 0, 0, 0};
+            this.executionCount = 0;
+            this.executeCountLabel.setText("0");
+
+            for (int i = 0; i < 4; ++i){
+                this.blockObservableLists[i].clear();
+            }
+            this.historyList.getItems().clear();
         }
     }
 
@@ -68,10 +303,10 @@ public class Controller {
     private TextField firstInstructionText;
 
     @FXML
-    private Button createListButton;
+    private ListView<Integer> instructionList;
 
     @FXML
-    private ListView<Integer> instructionList;
+    private Button createListButton;
 
     @FXML
     private Label currentInstructionLabel;
@@ -92,12 +327,6 @@ public class Controller {
     private Label pageMissingLabel;
 
     @FXML
-    private TableView memoryTable;
-
-    @FXML
-    private TableView historyTable;
-
-    @FXML
     private Label algLabel;
 
     @FXML
@@ -107,8 +336,22 @@ public class Controller {
     private Button lruButton;
 
     @FXML
-    private TableColumn memoryInfoColumn;
+    private ListView<Integer> block0List;
 
     @FXML
-    private TableColumn historyInfoColumn;
+    private ListView<Integer> block1List;
+
+    @FXML
+    private ListView<Integer> block2List;
+
+    @FXML
+    private ListView<Integer> block3List;
+
+    private ObservableList<Integer>[] blockObservableLists;
+
+    @FXML
+    private ListView<String> historyList;
+
+    @FXML
+    private Label executeCountLabel;
 }
